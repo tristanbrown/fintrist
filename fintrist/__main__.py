45418639c7de8cabec44062ab4571bed2845d43d
Tristan Brown <brown.tristan.r@gmail.com>
2018-02-05 14:30:41 -0600

    Reorganize as package

---
+++ fintrist/__main__.py
@@ -0,0 +1,22 @@
+"""
+Main routine of the package. 
+"""
+
+import sys
+from mypkg import config
+from mypkg import mymodule
+
+def main(args=None):
+    
+    # Determine inputs.
+    
+    if args is None:
+        args = sys.argv[1:]
+ 
+    
+    # Do something here. 
+    
+    
+    
+if __name__ == "__main__":
+    main()
