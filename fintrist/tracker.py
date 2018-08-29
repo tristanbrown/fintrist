bd41cb02a4c693ebeef4f537f519fc7094f60519
tristanbrown <brown.tristan.r@gmail.com>
2018-08-29 18:40:41 -0500

    Implement Study object

--- fintrist/tracker.py
+++ fintrist/tracker.py
@@ -4,8 +4,9 @@ events such as data refresh.
 """
 import time
 
-from fintrist.scrapers import equity
-from fintrist.settings import APIKEY
+from fintrist import settings
+from fintrist.analysis import Analysis, Study
+from fintrist.scrapers.base import Scraper
 
 class TrackingEngine():
     """Tracking engine that monitors and updates data acquired by scrapers.
@@ -17,28 +18,50 @@ class TrackingEngine():
     :param inputs: contains any parameters necessary for the scraper to run.
     :type inputs: dict
     """
-    def __init__(self, source, inputs):
+    def __init__(self, source, analysis_name, inputs):
+        self.study_list = []
         self.source = source
-        sources = ['stock']
-        if source == sources[0]:
-            self.scraper = equity.Equity(APIKEY, inputs['symbol'])
-        else:
-            raise ValueError(
-                "Choose an appropriate data-scraper. "
-                "Options: '{}'".format("', '".join(sources))
-            )
-        self.update()
+        self.scraper = Scraper(source, inputs).get()
+        self.analysis = Analysis(analysis_name, inputs)
+        self.track(settings.REFRESH_INTERVAL)
 
-    def track(self, interval=15):
+    def track(self, interval):
         """Periodically update the data and run any desired analyses."""
+        self.active_alerts = set()
         while True:
             print("{0}: Updating {1}".format(time.asctime(), self.source))
             self.update()
-            print(self.data)
             time.sleep(interval)
-    
+
     def update(self):
         """Update all of the tracked data."""
         self.scraper.refresh()
-        self.data = self.scraper.get_data()
+        data = self.scraper.get_data()
+        newstudy = self.create_study(data)
+        self.analysis.analyze(newstudy)
+        self.cycle_studies(newstudy)
+        self.handle_alerts(newstudy.alerts)
+        print(newstudy.data)
 
+    def create_study(self, data):
+        """Create a study object."""
+        study = Study(data)
+        study.save()
+        return study
+
+    def cycle_studies(self, study):
+        """Add to the study list, but don't let it get too long."""
+        self.study_list.append(study)
+        if len(self.study_list) > settings.KEEP_STUDIES:
+            self.study_list[0].delete()
+            self.study_list = self.study_list[1:]
+
+    def handle_alerts(self, alerts):
+        """Decide what to do with received alerts."""
+        alerts_on = alerts - self.active_alerts
+        alerts_off = self.active_alerts - alerts
+        print(
+            "New Alerts: {0}\n"
+            "Expired Alerts: {1}".format(alerts_on, alerts_off)
+        )
+        # self.active_alerts = self.active_alerts.update(alerts_on) - alerts_off
