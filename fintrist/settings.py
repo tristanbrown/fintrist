0a7aa2893fcf40ac96233022208da884313dca57
tristanbrown <brown.tristan.r@gmail.com>
2018-08-30 18:15:10 -0500

    Add AlertsBoard and directory constants

--- fintrist/settings.py
+++ fintrist/settings.py
@@ -2,7 +2,10 @@
 This is the config file for fintrist, containing various parameters the user may
 wish to modify. 
 """
+import os
 
 APIKEY = '***REMOVED***'
 REFRESH_INTERVAL = 15
 KEEP_STUDIES = 5
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
--- fintrist/tracker.py
+++ fintrist/tracker.py
@@ -2,9 +2,11 @@
 The Tracking Engine that monitors the wall clock and any alerts, triggering
 events such as data refresh.
 """
+import os
 import time
 
 from fintrist import settings
+from fintrist.alerts import AlertsBoard
 from fintrist.analysis import Analysis, Study
 from fintrist.scrapers.base import Scraper
 
@@ -19,7 +21,9 @@ class TrackingEngine():
     :type inputs: dict
     """
     def __init__(self, source, analysis_name, inputs):
+        self.basedir = self.generate_base_dir()
         self.study_list = []
+        self.alerts_board = AlertsBoard()
         self.source = source
         self.scraper = Scraper(source, inputs).get()
         self.analysis = Analysis(analysis_name, inputs)
@@ -27,7 +31,6 @@ class TrackingEngine():
 
     def track(self, interval):
         """Periodically update the data and run any desired analyses."""
-        self.active_alerts = set()
         while True:
             print("{0}: Updating {1}".format(time.asctime(), self.source))
             self.update()
@@ -40,8 +43,16 @@ class TrackingEngine():
         newstudy = self.create_study(data)
         self.analysis.analyze(newstudy)
         self.cycle_studies(newstudy)
-        self.handle_alerts(newstudy.alerts)
+        self.alerts_board.update(self.study_list)
         print(newstudy.data)
+
+    def generate_base_dir(self):
+        """Choose the directory in which to save the studies."""
+        datadir = settings.DATA_DIR
+        i = 0
+        while os.path.exists(os.path.join(datadir, str(i))):
+            i += 1
+        return os.path.join(datadir, str(i))
 
     def create_study(self, data):
         """Create a study object."""
@@ -55,13 +66,3 @@ class TrackingEngine():
         if len(self.study_list) > settings.KEEP_STUDIES:
             self.study_list[0].delete()
             self.study_list = self.study_list[1:]
-
-    def handle_alerts(self, alerts):
-        """Decide what to do with received alerts."""
-        alerts_on = alerts - self.active_alerts
-        alerts_off = self.active_alerts - alerts
-        print(
-            "New Alerts: {0}\n"
-            "Expired Alerts: {1}".format(alerts_on, alerts_off)
-        )
-        # self.active_alerts = self.active_alerts.update(alerts_on) - alerts_off
