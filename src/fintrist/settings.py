eaa6a0360087b22f70fbc8235f70aa19072112e4
tristanbrown <brown.tristan.r@gmail.com>
2019-02-16 19:02:07 -0600

    Use python-dotenv for sensitive data

---
+++ src/fintrist/settings.py
@@ -0,0 +1,11 @@
+"""
+This is the config file for fintrist, containing various parameters the user may
+wish to modify.
+"""
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+APIKEY = os.getenv('APIKEY')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
