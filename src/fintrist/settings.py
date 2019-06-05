76d12ff468d9cbe5e8c189a7b53f6ee3c53c4582
tristanbrown <brown.tristan.r@gmail.com>
2019-06-05 12:16:46 -0500

    Allow --local flag for DB access

--- src/fintrist/settings.py
+++ src/fintrist/settings.py
@@ -2,16 +2,26 @@
 This is the config file for fintrist, containing various parameters the user may
 wish to modify.
 """
+import argparse
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
+parser = argparse.ArgumentParser(description="Configuration options.")
+parser.add_argument('--local', action='store_true')
+args = parser.parse_args()
+
 class Config():
     APIKEY = os.getenv('APIKEY')
-    DATABASE_NAME = os.getenv('DB_NAME')
-    USERNAME = os.getenv('DB_USERNAME')
-    PASSWORD = os.getenv('DB_PASSWORD')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = int(os.getenv('DB_PORT'))
     APP_HOST = os.getenv('COMPUTERNAME')
+    DATABASE_NAME = os.getenv('DB_NAME')
+    if args.local:
+        USERNAME = None
+        PASSWORD = None
+        DB_HOST = 'localhost'
+    else:
+        USERNAME = os.getenv('DB_USERNAME')
+        PASSWORD = os.getenv('DB_PASSWORD')
+        DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = int(os.getenv('DB_PORT'))
