200883f1b0b02681e69a45d7a251913e58c18780
tristanbrown <brown.tristan.r@gmail.com>
2019-06-07 01:29:35 -0500

    Set package metadata

---
+++ __about__.py
@@ -0,0 +1,7 @@
+"""About fintrist and fintrist_app"""
+__name__ = 'fintrist'
+__version__ = '0.1.0'
+__author__ = "Tristan R. Brown",
+__email__ = "brown.tristan.r@gmail.com",
+__desc__ = "A personal financial analysis package.",
+__license__ = "Private",
