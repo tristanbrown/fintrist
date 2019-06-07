581fe8dc4f88b6cfeed0f1ccc3f96d35417ffddb
tristanbrown <brown.tristan.r@gmail.com>
2019-06-07 00:11:15 -0500

    Change local specification to env variable

--- src/fintrist/settings.py
+++ src/fintrist/settings.py
@@ -2,21 +2,17 @@
 This is the config file for fintrist, containing various parameters the user may
 wish to modify.
 """
-import argparse
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-parser = argparse.ArgumentParser(description="Configuration options.")
-parser.add_argument('--local', action='store_true')
-args = parser.parse_args()
-
 class Config():
     APIKEY = os.getenv('APIKEY')
     APP_HOST = os.getenv('COMPUTERNAME')
     DATABASE_NAME = os.getenv('DB_NAME')
-    if args.local:
+    local = int(os.getenv('DB_LOCAL'))
+    if local:
         USERNAME = None
         PASSWORD = None
         DB_HOST = 'localhost'
