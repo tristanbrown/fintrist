c0451d478f48b5091a1072c5bda724595917613c
tristanbrown <brown.tristan.r@gmail.com>
2019-06-22 20:06:27 -0500

    Eliminate Stream model

--- src/fintrist/models.py
+++ src/fintrist/models.py
@@ -14,7 +14,6 @@ from mongoengine.fields import (
     EmbeddedDocumentListField, IntField, FileField,
     ListField, MapField, ReferenceField, StringField,
 )
-from mongoengine.errors import DoesNotExist
 from mongoengine import signals
 from apscheduler.jobstores.base import JobLookupError
 from bson.dbref import DBRef
@@ -24,7 +23,7 @@ from fintrist import processes, util
 from fintrist.scheduling import scheduler
 from fintrist.notify import Notification
 
-__all__ = ('Stream', 'Study', 'Process', 'Trigger')
+__all__ = ('Study', 'Process', 'Trigger')
 
 logger = logging.getLogger(__name__)
 
@@ -117,151 +116,6 @@ class Trigger(EmbeddedDocument):
     def fire(self, **kwargs):
         """Trigger the specified actions."""
         Notification(self.actions, **kwargs)
-
-class Stream(Document):
-    """Hosts the Studies, running them periodically.
-
-    Usage: Stream(name, refresh)
-    """
-    # pylint: disable=no-member
-    name = StringField(max_length=120, required=True, unique=True)
-    refresh = IntField(min_value=15)
-    studies = ListField(ReferenceField('Study'))
-
-    # Meta
-    schema_version = IntField(default=1)
-    meta = {'strict': False}
-
-    def clean(self):
-        """Clean out invalid data."""
-        for study in self.studies:
-            if isinstance(study, DBRef):
-                self.studies.remove(study)
-
-    def create_study(self, name, proc_name, inputs):
-        """Create a new study tracked by a Stream instance."""
-        try:
-            self.reload()
-        except DoesNotExist as ex:
-            logger.error("Please save the Stream before trying to create a Study.")
-            raise ex
-        proc_ref = Process.objects(name=proc_name).get().id
-        newstudy = Study(name, proc_ref, inputs)
-        newstudy.save()
-        self.add_study(newstudy)
-
-    def add_study(self, newstudy):
-        """Add an existing Study to the Stream."""
-        self.update(add_to_set__studies=newstudy)
-        self.reload()
-
-    def remove_study(self, oldstudy):
-        """Remove a Study from the Stream."""
-        self.update(pull__studies=oldstudy)
-        self.reload()
-
-    def list_studies(self):
-        """List the studies associated with this stream, by name."""
-        return [study.name for study in self.studies]
-
-    def move_study_to_idx(self, a_study, newidx):
-        """Move a Study to a specific index in the Study list."""
-        idx = self.studies.index(a_study)
-        self.studies.pop(idx)
-        self.studies.insert(newidx, a_study)
-        self.save()
-        self.reload()
-
-    def move_study_earlier(self, a_study):
-        """Move a Study one spot earlier in the Study list."""
-        idx = self.studies.index(a_study)
-        try:
-            self.move_study_to_idx(a_study, idx - 1)
-        except IndexError:
-            pass
-
-    def move_study_later(self, a_study):
-        """Move a Study one spot later in the Study list."""
-        idx = self.studies.index(a_study)
-        try:
-            self.move_study_to_idx(a_study, idx + 1)
-        except IndexError:
-            pass
-
-    def move_study_first(self, a_study):
-        """Move a Study to the front of the Study list."""
-        self.move_study_to_idx(a_study, 0)
-
-    def move_study_last(self, a_study):
-        """Move a Study to the front of the Study list."""
-        lastidx = len(self.studies)
-        self.move_study_to_idx(a_study, lastidx)
-
-    def rename(self, newname):
-        """Rename the Stream."""
-        self.name = newname
-        self.save()
-        self.reload()
-        if self.active:
-            scheduler.modify_job(str(self.id), name=self.name)
-
-    def update_refresh(self, newrefresh):
-        """Update the refresh interval."""
-        self.refresh = newrefresh
-        self.save()
-        self.reload()
-        if self.active:
-            scheduler.reschedule_job(str(self.id), trigger='interval', seconds=self.refresh)
-
-    def run(self):
-        """Update the Study instances and collect alerts.
-
-        Each Study will return alerts as a list of strings.
-        """
-        self.reload()
-        logger.warning("%s: Updating %s", dt.datetime.now(tzlocal()), self.name)
-        for study in self.studies:
-            study.run()
-
-    @classmethod
-    def run_stream(cls, stream_id):
-        """Static wrapper for `run`."""
-        this_stream = cls.objects(id=stream_id).get()
-        this_stream.run()
-
-    def activate(self):
-        """Periodically run the Stream."""
-        scheduler.add_job(
-            self.run_stream,
-            args=[str(self.id)],
-            trigger='interval',
-            seconds=self.refresh,
-            id=str(self.id),
-            name=self.name,
-            replace_existing=True,
-        )
-
-    def deactivate(self):
-        """Stop the Stream from running periodically."""
-        try:
-            scheduler.remove_job(str(self.id))
-        except JobLookupError:
-            print("Job not found")
-
-    def run_stream_once(self):
-        """Submit a job to run the whole stream once."""
-        scheduler.add_job(
-            self.run_stream,
-            args=[str(self.id)],
-            id=str(self.id) + 'once',
-            name=self.name + ' once',
-            replace_existing=False,
-        )
-
-    @property
-    def active(self):
-        """Boolean value of whether the Stream is active in the scheduler."""
-        return bool(scheduler.get_job(str(self.id)))
 
 @util.handler(signals.pre_delete)
 def clean_files(sender, document):  #pylint: disable=unused-argument
