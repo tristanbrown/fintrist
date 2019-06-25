9385e6b77ebf7acfbfbb7017c75960a4123517dd
tristanbrown <brown.tristan.r@gmail.com>
2019-06-25 02:20:32 -0500

    Add date-sampling logic

--- src/fintrist/processes/analysis.py
+++ src/fintrist/processes/analysis.py
@@ -1,9 +1,12 @@
 """
 The engine that applies analyses to data and generates alerts.
 """
+import numpy as np
 import pandas as pd
+import datetime as dt
+import dateutil
 
-__all__ = ['any_data', 'moving_avg']
+__all__ = ['any_data', 'moving_avg', 'sample_dates',]
 
 def any_data(data):
     """If there is data, raise alert.
@@ -28,3 +31,18 @@ def moving_avg(data):
     outdf['100-day avg'] = outdf['close_price'].rolling(100, center=centering).mean()
     alerts = ['complete']
     return (outdf, alerts)
+
+def sample_dates(data, N=100, window=365, backdate=0):
+    """Sample the available dates in the data.
+    ::parents:: data
+    ::params:: N, window, backdate
+    """
+    if isinstance(backdate, int):
+        backdt = dt.datetime.now() - dt.timedelta(days=backdate)
+    else:
+        backdt = dateutil.parser.parse(backdate)
+    interval = data.index[
+        (data.index > backdt - dt.timedelta(days=window)) & (data.index <= backdt)]
+    sample_idx = np.random.choice(interval, N, replace=False)
+    sample_idx.sort()
+    return sample_idx
