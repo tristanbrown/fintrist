7903e0573648ed174f56483e638a9d97c9531d73
tristanbrown <brown.tristan.r@gmail.com>
2020-11-19 23:29:18 -0800

    Update plotting helper function

--- src/fintrist_ds/viz.py
+++ src/fintrist_ds/viz.py
@@ -1,12 +1,12 @@
 """Visualizations"""
 import datetime
 
-from fintrist import get_data, get_study, spawn_stream
+from fintrist import get_data, get_study, spawn_study
 from .engine import schedule_study
 from .services import backtest_and_sim, compare_sims
 
 def plot_stream(str_name, window='1y', **kwargs):
-    study = spawn_stream(str_name, **kwargs)[-1]
+    study = spawn_study(str_name, **kwargs)
     schedule_study(study.name, force=False)  # NOTE: check if this is finished?
     study = get_study(study.name)  # Refresh the study with new data.
     print(f"Alerts: {study.alerts}")
@@ -18,7 +18,7 @@ def plot_stream(str_name, window='1y', **kwargs):
     data.plot(title=study.name)
 
 def plot_sma(symbol, window='1y'):
-    return plot_stream('Stock SMA', window, symbol=symbol)
+    return plot_stream('MovingAvg', window, symbol=symbol)
 
 def plot_benchmark(symbol, model, strategy, period='5y'):
     sim = backtest_and_sim(symbol, model, strategy, period)
