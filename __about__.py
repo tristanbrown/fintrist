38875e4f2c3ab4db31195c798c4262a2f51e4a22
tristanbrown <brown.tristan.r@gmail.com>
2021-03-21 23:59:23 -0700

    Bump to version 1.5.0

--- __about__.py
+++ __about__.py
@@ -1,6 +1,6 @@
 """About fintrist and fintrist_app"""
 __name__ = 'fintrist'
-__version__ = '1.4.0'
+__version__ = '1.5.0'
 __author__ = "Tristan R. Brown",
 __email__ = "brown.tristan.r@gmail.com",
 __desc__ = "A personal financial analysis package.",
